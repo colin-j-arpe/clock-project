486c497f0d3a7eec8edb47e079a59d98cd5e0c76
Colin Arpe <colin.j.arpe@outlook.com>
2016-11-17 15:28:05 -0500

    Add JS function to increment and display time

--- scripts.js
+++ scripts.js
@@ -3,9 +3,33 @@ var hour = now.getHours();
 var minute = now.getMinutes();
 var second = now.getSeconds();
 var clock = document.getElementById("clock");
+var screen = document.getElementsByTagName("body")[0];
+var counter;
 
-window.setInterval (updateClock, 1000);
+window.setInterval (updateClock, 2000);
 
 function updateClock ()	{
+	now = new Date();
+	hour = now.getHours();
+	minute = now.getMinutes();
+	second = now.getSeconds();
 	clock.innerText	= (hour + ":" + minute + ":" + second);
+	reColour(hour, minute, second);
 }
+
+function reColour (r, g, b)	{
+	var colourString = "rgb (" + r + "," + g + "," + b + ")"
+	console.log(colourString);
+	screen.style.backgroundColor = colourString;
+}
+
+// setTimeout (updateClock, 2000);
+
+// function updateClock ()	{
+// 	now = new Date();
+// 	hour = now.getHours();
+// 	minute = now.getMinutes();
+// 	second = now.getSeconds();
+// 	clock.innerText	= (hour + ":" + minute + ":" + second);
+// 	window.setTimeout (updateClock, 1000);
+// }
